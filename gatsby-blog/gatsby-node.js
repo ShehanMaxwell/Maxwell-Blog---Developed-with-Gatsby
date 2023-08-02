cb2f2abef3094f4d8c5e838c699bde4c6244d309
Shehan Maxwell <shehanbbn@gmail.com>
2023-08-02 23:15:00 +0530

    changed 3 files

--- gatsby-blog/gatsby-node.js
+++ gatsby-blog/gatsby-node.js
@@ -1,3 +1,43 @@
+// const { createFilePath } = require(`gatsby-source-filesystem`)
+
+// exports.onCreateNode = ({ node, getNode, actions }) => {
+//   const { createNodeField } = actions
+//   if (node.internal.type === `MarkdownRemark`) {
+//     const slug = createFilePath({ node, getNode })
+
+//     createNodeField({
+//       node,
+//       name: `slug`,
+//       value: slug,
+//     })
+//   }
+// }
+
+// export.createPages = ({graphql, actions}) => {
+//   const {createPage} = actions
+//   return graphql(`
+//     {
+//       allMarkdownRemark {
+//         edges {
+//           node {
+//             fields {
+//               slug
+//             }
+//           }
+//         }
+//       }
+//     }
+
+//   `).then(result => {
+//     result.data.allMarkdownRemark.edges.forEach(({node})=>{
+//       createPage({
+//         path: node.fields.slug,
+//         component:
+//       })
+//     })
+//   })
+// }
+const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -11,4 +51,32 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
       value: slug,
     })
   }
+}
+
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.fields.slug,
+        component: require.resolve("./src/templates/blog-post.js"),
+        context: {
+          // Optional context data you can pass to the template component.
+          slug: node.fields.slug,
+        },
+      })
+    })
+  })
 }
